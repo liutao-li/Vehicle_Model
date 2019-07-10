d9845aa63390b38598e16847c33cfdaa87b5e296
liutli2019 <46802187+liutli2019@users.noreply.github.com>
2019-07-10 15:49:41 +0800

    Add files via upload

---
+++ Kinematic_model.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+class Bicycle():
+    def __init__(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta = 0
+        self.beta = 0
+
+        self.L = 2
+        self.lr = 1.2
+        self.w_max = 1.22
+
+        self.sample_time = 0.01
+
+    def reset(self):
+        self.xc = 0
+        self.yc = 0
+        self.theta = 0
+        self.delta
+        self.beta = 0
+
+    def step(self, v, w):
+        if w > self.w_max:
+            w = self.w_max
+        self.xc = self.xc + v * np.cos(self.theta + self.beta) * self.sample_time
+        self.yc = self.yc + v * np.sin(self.theta + self.beta) * self.sample_time
+        self.theta = self.theta + v * np.cos(self.beta) * np.tan(self.delta) / self.L * self.sample_time
+        self.delta = self.delta + w * self.sample_time
+        self.beta = np.arctan(self.lr * np.tan(self.delta) / self.L)
+        pass
