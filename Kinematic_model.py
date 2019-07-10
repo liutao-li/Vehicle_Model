1fd52da3fa6a16f04350d8e67cc36e420de4bbd4
liutli2019 <46802187+liutli2019@users.noreply.github.com>
2019-07-10 15:59:54 +0800

    Update Kinematic_model.py

--- Kinematic_model.py
+++ Kinematic_model.py
@@ -4,15 +4,15 @@ import matplotlib.image as mpimg
 
 class Bicycle():
     def __init__(self):
-        self.xc = 0
+        self.xc = 0  # the desired point at the center of gravity
         self.yc = 0
-        self.theta = 0
+        self.theta = 0  #the heading angle(rad)
         self.delta = 0
-        self.beta = 0
+        self.beta = 0  #the side slip angle(rad)
 
-        self.L = 2
-        self.lr = 1.2
-        self.w_max = 1.22
+        self.L = 2  #the length of wheelbase
+        self.lr = 1.2  #the distance between the center of  mass and the rear axle
+        self.w_max = 1.22  #w_max:the maximum of steering rate
 
         self.sample_time = 0.01
 
@@ -25,7 +25,7 @@ class Bicycle():
 
     def step(self, v, w):
         if w > self.w_max:
-            w = self.w_max
+            w = self.w_max 
         self.xc = self.xc + v * np.cos(self.theta + self.beta) * self.sample_time
         self.yc = self.yc + v * np.sin(self.theta + self.beta) * self.sample_time
         self.theta = self.theta + v * np.cos(self.beta) * np.tan(self.delta) / self.L * self.sample_time
